f2f8c3d8d5ac1be3c2d3e3eafa89d4964fa88e04
EmanuelCastelo <jescbf@yahoo.com.br>
2021-01-05 20:48:07 -0300

    Saving a new Department

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -7,11 +7,19 @@ import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
-	
+
 	private DepartmentDao dao = DaoFactory.createDepartmentDao();
-	
-	public List<Department> findAll(){
+
+	public List<Department> findAll() {
 		return dao.findAll();
+	}
+
+	public void SaveOrUpdate(Department obj) {
+		if (obj.getId() == null) {
+			dao.insert(obj);
+		} else {
+			dao.update(obj);
+		}
 	}
 
 }
